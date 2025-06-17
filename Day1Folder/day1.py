f696becbd7c93bd58e3134eebd69f948d45c9f30
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-17 01:32:01 +0000

    Jun 17, 2025, 1:32 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,11 +1,19 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
+# print("hello from day1")
 
+# print("My name is Ms Liang")
+# print("I love coding")
 ########################################################################
 # Task 1:
 
-
+# my_wallet10 = 10
+# myPinkWallet = 1000
+# print( myPinkWallet + 3.50 )
+# print( myPinkWallet - 3.50 )
+# print( myPinkWallet * 3 )
+# print( 2 ** 3 )
+# print( myPinkWallet / 3.50 )
 
 ########################################################################
 # Task 2:
@@ -15,12 +23,20 @@ print("hello from day1")
 ########################################################################
 # Task 3:
 
-
+# animal1 = "tiger"
+# animal2 = "elephant"
+# animal3 = "lion"
+# print(animal1)
+# print(animal2)
+# print(animal3)
 
 ########################################################################
 # Task 4:
 
-
+name = "Santa Claus"
+message = "Let's go to the playground"
+price = 100
+print(name + " says " + message + str(price))
 
 ########################################################################
 # Task 5:
@@ -30,11 +46,21 @@ print("hello from day1")
 ########################################################################
 # Task 6:
 
+# def intro(newfriend):
+#     print("My name is Ms Liang")
+#     print("I love coding")
+#     print("nice to meet you, " + newfriend)
 
-
+# intro("Mary")
+# intro("Peter")
 ########################################################################
 # Task 7:
 
+def addition(num1, num2):
+    result = num1 + num2
+    print(result)
+
+addition(414,3)
 
 
 ########################################################################
