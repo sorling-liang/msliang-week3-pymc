be6aec8ce82bb6c9762fe693066f700133b65b20
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2025-06-18 01:53:17 +0000

    Jun 18, 2025, 1:53 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -2,7 +2,7 @@
 # COMMENT out the previous task before going on to the next task
 print("hello from day3")
 
-yourname = input("what is your name")
+yourname = input("what is your name?")
 
 ########################################################################
 # Task 1:
